31747e1ec89a7eed6f49501c40535f6bfe8224fd
Phi Dinh <phi.dinh@cyberlogitec.com>
2023-12-15 14:15:42 +0700

    refactor: search task feature

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import { Input } from "../ui/input";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { PencilIcon } from "@heroicons/react/24/solid";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import UploadAvatarPopup from "../popup/UploadAvatarPopup";
 import { useSession } from "next-auth/react";
+import Logo from "@/assets/icons/logo";
+import SearchBar from "../task-board/SearchBar";
 
 const Header = () => {
   const imageUrl = "https://github.com/shadcn.png";
@@ -21,21 +21,11 @@ const Header = () => {
       <div className="fixed left-0 right-0 z-50 bg-white">
         <header className="flex items-center justify-center">
           <div className="mt-7 flex min-w-[260px] items-center justify-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M24.7488 14.1421L14.1422 3.53553L3.53555 14.1421L14.1422 24.7487L24.7488 14.1421ZM14.1422 0L1.54972e-05 14.1421L14.1422 28.2843L28.2843 14.1421L14.1422 0Z"
-                fill="#2563DC"
-              />
-            </svg>
+            <Logo />
             <span className="text-xl font-medium">TaskBan</span>
           </div>
           <div className="flex flex-1 flex-wrap items-center gap-20 border-b border-l border-solid">
-            <div className="relative flex-1 py-3 pl-12">
-              <Input placeholder="Search" />
-              <MagnifyingGlassIcon className="absolute bottom-0 right-3 top-0 my-auto h-6 w-6 text-gray-500" />
-            </div>
+            <SearchBar />
             <div className="group relative mr-10 h-11 w-11 cursor-pointer rounded-full bg-gray-300">
               <Avatar className="m-auto h-full w-full" onClick={() => setShowPopup(true)}>
                 <AvatarImage src={data?.user?.avatar ?? imageUrl} />
--- src/stores/useGeneralStore.ts
+++ src/stores/useGeneralStore.ts
@@ -4,16 +4,20 @@ import { devtools } from "zustand/middleware";
 
 interface Tasks {
   draggedTask: TaskInfo | null;
+  taskKeywork: string;
 }
 
 interface Actions {
   setDraggedTask: (dragTask: TaskInfo | null) => void;
+  setTaskKeyword: (keyword: string) => void;
 }
 
 // persist store
 export const useGeneralStore = create<Tasks & Actions>()(
   devtools((set) => ({
     draggedTask: null,
+    taskKeywork: "",
+    setTaskKeyword: (keyword: string) => set((_) => ({ taskKeywork: keyword })),
     setDraggedTask: (dragTask) => set((_) => ({ draggedTask: dragTask })),
   })),
 );
